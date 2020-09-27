7bd4b58981fe208bd14b0ffddf79d730f87db459
Enrico <enrico.mazzarella@gmail.com>
2020-09-27 14:58:02 +0200

    personal data

--- mysql/scripts/insertData.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO emp_profile (username, password, email, first_name, last_name)
-VALUES ('enricomzz', 'enricomzz', 'enrico.mazzarella@gmail.com','Enrico','Mazzarella'),
-       ('lucamzz', 'lucamzz', 'luca.mazzarella@email.com','Luca','Mazzarella');
-	   
